37eab555c201748c8a1ebf78a4c42473638d03d4
Toussaint Thomas <toussaint.h.thomas@gmail.com>
2025-04-05 14:27:27 -0400

    Convert hero image animation to its own component

---
+++ src/ui/hero-animation/HeroAnimation.jsx
@@ -0,0 +1,95 @@
+'use client'
+
+import styles from '@/ui/hero-animation/HeroAnimation.module.scss'
+import Image from 'next/image'
+
+export default function HeroAnimation({ launched }) {
+    return (
+        <div className={`${styles.backgroundAnim}`}>
+            <div className={styles.cloudsTop}>
+                {[...Array(2)].map((_, i) => (
+                    <div key={i} className={styles.cloudsBg}></div>
+                ))}
+            </div>
+            <div className={`${styles.moonIMG}`}>
+                <div>
+                    <Image
+                        src="/CresantMoon.svg"
+                        alt="Moon"
+                        width={70}
+                        height={70}
+                        unoptimized
+                    />
+                </div>
+            </div>
+            <div className={`${styles.cloudsBottom}`}>
+                <div>
+                    <div className={`${styles.cloudsBg2}`}></div>
+                    <div className={`${styles.cloudsBg3}`}></div>
+                </div>
+                <div>
+                    <div className={`${styles.cloudsBg2}`}></div>
+                    <div className={`${styles.cloudsBg3}`}></div>
+                </div>
+            </div>
+            <div className={`${styles.mountainRange}`}>
+                <Image
+                    src="/MountainRange.svg"
+                    alt="Mountain Range"
+                    width={2000}
+                    height={300}
+                    unoptimized
+                    className={styles.mountainRangeImg}
+                />
+            </div>
+            <div className={`${styles.launchPadContainer}`}>
+                <div>
+                    <div className={`${styles.launchPad}`}>
+                        <Image
+                            src={'/LaunchPad.svg'}
+                            alt={'n/a'}
+                            width={2000}
+                            height={300}
+                            unoptimized={true}
+                            className={`${styles.shuttleLaunchPad}`}
+                        />
+                        <div
+                            className={`${styles.spaceShuttle} ${launched ? styles.launched : ''}`}
+                        >
+                            <Image
+                                src={'/SpaceShuttle.svg'}
+                                alt={'n/a'}
+                                width={2000}
+                                height={300}
+                                unoptimized={true}
+                            />
+                            <div
+                                className={`${styles.flameContainer} ${launched ? styles.showFlames : ''}`}
+                            >
+                                {Array.from({ length: 5 }).map((_, i) => (
+                                    <Image
+                                        key={i}
+                                        src="/RocketFlame.svg"
+                                        alt="Rocket Flame"
+                                        width={100}
+                                        height={100}
+                                        unoptimized
+                                        className={styles.rocketFlame}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                        <Image
+                            src={'/ShuttleElevator.svg'}
+                            alt={'n/a'}
+                            width={2000}
+                            height={300}
+                            unoptimized={true}
+                            className={`${styles.shuttleElevator}`}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/ui/page-header/PageHeader.jsx
+++ src/ui/page-header/PageHeader.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import styles from './PageHeader.module.scss'
 import Image from 'next/image'
 import HeaderText from './HeaderText.json'
+import HeroAnimation from '@/ui/hero-animation/HeroAnimation'
 
 export default function PageHeader() {
     const [launched, setLaunched] = useState(false)
@@ -24,91 +25,7 @@ export default function PageHeader() {
 
     return (
         <header className={`${styles.pageHeader} flex`}>
-            <div className={`${styles.backgroundAnim}`}>
-                <div className={`${styles.cloudsTop}`}>
-                    <div className={`${styles.cloudsBg}`}></div>
-                    <div className={`${styles.cloudsBg}`}></div>
-                </div>
-                <div className={`${styles.moonIMG}`}>
-                    <div>
-                        <Image
-                            src="/CresantMoon.svg"
-                            alt="Moon"
-                            width={70}
-                            height={70}
-                            unoptimized
-                        />
-                    </div>
-                </div>
-                <div className={`${styles.cloudsBottom}`}>
-                    <div>
-                        <div className={`${styles.cloudsBg2}`}></div>
-                        <div className={`${styles.cloudsBg3}`}></div>
-                    </div>
-                    <div>
-                        <div className={`${styles.cloudsBg2}`}></div>
-                        <div className={`${styles.cloudsBg3}`}></div>
-                    </div>
-                </div>
-                <div className={`${styles.mountainRange}`}>
-                    <Image
-                        src="/MountainRange.svg"
-                        alt="Mountain Range"
-                        width={2000}
-                        height={300}
-                        unoptimized
-                        className={styles.mountainRangeImg}
-                    />
-                </div>
-                <div className={`${styles.launchPadContainer}`}>
-                    <div>
-                        <div className={`${styles.launchPad}`}>
-                            <Image
-                                src={'/LaunchPad.svg'}
-                                alt={'n/a'}
-                                width={2000}
-                                height={300}
-                                unoptimized={true}
-                                className={`${styles.shuttleLaunchPad}`}
-                            />
-                            <div
-                                className={`${styles.spaceShuttle} ${launched ? styles.launched : ''}`}
-                            >
-                                <Image
-                                    src={'/SpaceShuttle.svg'}
-                                    alt={'n/a'}
-                                    width={2000}
-                                    height={300}
-                                    unoptimized={true}
-                                />
-                                <div
-                                    className={`${styles.flameContainer} ${launched ? styles.showFlames : ''}`}
-                                >
-                                    {Array.from({ length: 5 }).map((_, i) => (
-                                        <Image
-                                            key={i}
-                                            src="/RocketFlame.svg"
-                                            alt="Rocket Flame"
-                                            width={100}
-                                            height={100}
-                                            unoptimized
-                                            className={styles.rocketFlame}
-                                        />
-                                    ))}
-                                </div>
-                            </div>
-                            <Image
-                                src={'/ShuttleElevator.svg'}
-                                alt={'n/a'}
-                                width={2000}
-                                height={300}
-                                unoptimized={true}
-                                className={`${styles.shuttleElevator}`}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <HeroAnimation launched={launched}></HeroAnimation>
             <div className={`${styles.heroText}`}>
                 <div>
                     <div>
