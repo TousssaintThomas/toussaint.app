43f208e36d7b67978003738b648be6be5a944a1d
Toussaint Thomas <toussaint.h.thomas@gmail.com>
2025-04-16 09:46:31 -0400

    Implemented navigation bar animations with motion js

--- src/ui/nav-bar/NavBar.jsx
+++ src/ui/nav-bar/NavBar.jsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import { useState } from 'react'
 import styles from './navBar.module.scss'
+import { motion, useInView, AnimatePresence } from 'motion/react'
 
 // Icons
 import MenuIcon from '@mui/icons-material/Menu'
@@ -61,6 +62,12 @@ export default function NavBar() {
         )
     })
 
+    const fadeVariants = {
+        hidden: { opacity: 0, pointerEvents: 'none' },
+        visible: { opacity: 1, pointerEvents: 'auto' },
+        exit: { opacity: 0, transition: { duration: 0.1 } }
+    }
+
     return (
         <nav
             className={`${styles['nav-bar']} ${menuOpen ? styles['menu-open'] : ''}`}
@@ -84,10 +91,21 @@ export default function NavBar() {
                     </div>
                 </button>
             </div>
-            <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-                <ul>{navList}</ul>
-                <ul>{offsiteList}</ul>
-            </div>
+            <AnimatePresence>
+                {menuOpen && (
+                    <motion.div
+                        className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8"
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                        variants={fadeVariants}
+                        transition={{ duration: 0.15 }}
+                    >
+                        <ul>{navList}</ul>
+                        <ul>{offsiteList}</ul>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </nav>
     )
 }
